75417ed6636000842d9378e7a002ba4439447b6b
xinglong.xia <xinglong.xia@rakuten.com>
2016-02-19 17:45:32 +0800

    update main file

--- src/MainGame.java
+++ src/MainGame.java
@@ -3,6 +3,8 @@ public class MainGame {
 
     public static void main(String[] args) {
         System.out.println("Welcome to the SouthPark!");
+        int index = 100;
+        System.out.println(index);
     }
 
 }
