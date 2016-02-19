d54adab87ede3bb0ac52eaae399f1a76104ba1ae
Kevinxxl <xiaxinglong@hotmail.com>
2016-02-19 17:31:23 +0800

    edit by other person in master branch

--- src/MainGame.java
+++ src/MainGame.java
@@ -3,6 +3,7 @@ public class MainGame {
 
     public static void main(String[] args) {
         System.out.println("Welcome to the SouthPark!");
+        System.out.println("Edit by other person!");
     }
 
 }
